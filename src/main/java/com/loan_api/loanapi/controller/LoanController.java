7a6c443a3af134b2c116823321cbac30a06f4705
ze-fernando <jfenginner935@gmail.com>
2023-09-02 17:41:45 -0300

    create controller

---
+++ src/main/java/com/loan_api/loanapi/controller/LoanController.java
@@ -0,0 +1,49 @@
+package com.loan_api.loanapi.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.loan_api.loanapi.entity.Costumer;
+import com.loan_api.loanapi.entity.Loan;
+import com.loan_api.loanapi.entity.Response;
+import com.loan_api.loanapi.service.LoanService;
+
+
+@RestController
+@Controller
+public class LoanController {
+ 
+
+    @PostMapping("/customer-loans")
+    public Response customer(@RequestBody Costumer c){
+
+        boolean valid = c.getLocation().toUpperCase().equals("SP") &&
+            c.getAge() < 30 &&
+            c.getIncome() <= 5000;
+
+        List<Loan> list = new ArrayList<>();
+
+        if (c.getIncome() <= 3000) LoanService.personalLoan(list);
+
+        if (valid) LoanService.personalLoan(list);
+
+        if (c.getIncome() >= 5000) LoanService.consignmentLoan(list);
+
+        if (c.getIncome() <= 3000) LoanService.guaranteedLoan(list); 
+
+        if (valid) LoanService.guaranteedLoan(list); 
+
+        
+        Response response = new Response(c.getName(), list);
+        
+
+        return response;
+    }
+
+}
